8dd792312b2ac8a37a2ff7955d242fe79af154f6
Fannusz <121514922+Fannusz@users.noreply.github.com>
2023-01-24 15:44:06 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  //crie um sprite de trex
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  
+  //adicione dimensão e posição ao trex
+  trex.scale = 0.5;
+  trex.x = 50
+  
+  //crie um sprite ground (solo)
+  ground = createSprite(200,180,400,20);
+ 
+}
+
+function draw() {
+  background(220);
+ 
+  //pular quando a tecla espaço for pressionada
+  if(keyDown("space")) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+ 
+ //impedir que o trex caia 
+  trex.collide(ground);
+  drawSprites();
+}
